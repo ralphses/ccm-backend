fec95c0655ee8261db6fa48c9dd0fb956edf1e52
Raphael Eze <eze.raph@gmail.com>
2023-08-30 17:25:13 +0100

    first commit

---
+++ src/main/java/com/example/ccmbackend/CcmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ccmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ccmbackend/CcmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ccmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
